70e1ebffb3df808e1c6bf5f8c4fff699857802fa
thiagosngouvea <thiagosngouvea@gmail.com>
2023-03-08 20:46:50 -0300

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,11 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main>
+    <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <Component {...pageProps} />
+    </div>
+  </main>
+  )
 }
