93f2df0c49f71363c85e0fce3ad562bfb394dde0
thiagosngouvea <thiagosngouvea@gmail.com>
2023-03-10 21:31:05 -0300

    feat: :sparkles: Botãoa gora recebe props

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 
-interface ButtonProps {
+
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   onClick: () => void;
   className?: string;
   children: React.ReactNode;
+  type: "button" | "submit" | "reset";
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, className, children }) => {
+const Button: React.FC<ButtonProps> = ({ onClick, className, children, type = "button", ...rest }) => {
   return (
     <button
       className={`px-3 py-2 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600 ${className}`}
       onClick={onClick}
+      type={type}
+      {...rest}
     >
       {children}
     </button>
